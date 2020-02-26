541483af92b7f3dd15fa26b825ac27d345d62164
mrkaliv <kaliv_farstryder@hotmail.com>
2020-02-26 09:11:27 -0500

    Added datasets and documented them.  Also changes the logreg code to ensure Transition is going into the formula correctly.  The actions need to take colnames first then output the first element in the vector.

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' @title Sample Nation-state Statistics
+#' @docType data
+#' @name Test_Data
+#' @family data-testing
+#' @format A \code{tibble} with 60 rows and 6 variables
+#' \tabular{rlll}{
+#' [,1] \tab Transition \tab Binary markov chain indicator for transitioning to conflic \tab \bold{numeric}\cr
+#' [,2] \tab GDP \tab Gross Domestic Product \tab \bold{numeric}\cr
+#' [,3] \tab IMR \tab Infant Mortality Rate \tab \bold{numeric}\cr
+#' [,4] \tab PD \tab Population Density \tab \bold{numeric}\cr
+#' [,5] \tab FT2Y \tab Two Year Freedom Trend \tab \bold{numeric}\cr
+#' [,6] \tab FT5Y \tab Five Year Freedom Trend \tab \bold{numeric}
+#' }
+#' @description A dataset containing a sample of statistics from Arab nation states between 2004 and 2009.
+NULL
+
+#' @title Sample Nation-state Statistics Subset
+#' @docType data
+#' @name Test_Data_Small
+#' @family data-testing
+#' @format A \code{tibble} with 60 rows and 3 variables
+#' \tabular{rlll}{
+#' [,1] \tab Transition \tab Binary markov chain indicator for transitioning to conflic \tab \bold{numeric}\cr
+#' [,2] \tab GDP \tab Gross Domestic Product \tab \bold{numeric}\cr
+#' [,3] \tab IMR \tab Infant Mortality Rate \tab \bold{numeric}\cr
+#' }
+#' @description A dataset containing a subset of (Test_Data) sample of statistics from Arab nation states between 2004 and 2009.
+NULL
